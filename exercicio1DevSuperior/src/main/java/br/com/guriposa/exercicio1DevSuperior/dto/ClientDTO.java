1665b92706ba1355544d1e6d799bb4964425d47f
Gustavo <guriposa@gmail.com>
2022-08-17 23:07:01 -0300

    Change Lombok because STS doesn't accept it like IntelliJ

--- exercicio1DevSuperior/src/main/java/br/com/guriposa/exercicio1DevSuperior/dto/ClientDTO.java
+++ exercicio1DevSuperior/src/main/java/br/com/guriposa/exercicio1DevSuperior/dto/ClientDTO.java
@@ -1,23 +1,76 @@
 package br.com.guriposa.exercicio1DevSuperior.dto;
 
 import br.com.guriposa.exercicio1DevSuperior.entities.Client;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
-
 public class ClientDTO {
+
+    public ClientDTO() {}
+
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
     private Long id;
     private String name;
     private String cpf;
     private Double income;
     private Instant birthDate;
     private Integer children;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
 
     public ClientDTO(Client client){
         this.id = client.getId();
--- exercicio1DevSuperior/src/main/java/br/com/guriposa/exercicio1DevSuperior/entities/Client.java
+++ exercicio1DevSuperior/src/main/java/br/com/guriposa/exercicio1DevSuperior/entities/Client.java
@@ -1,20 +1,21 @@
 package br.com.guriposa.exercicio1DevSuperior.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
+    public Client(){}
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,6 +40,70 @@ public class Client implements Serializable {
     @PreUpdate
     public void preUpdate(){
         updatedAt = Instant.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
     }
 
     @Override
